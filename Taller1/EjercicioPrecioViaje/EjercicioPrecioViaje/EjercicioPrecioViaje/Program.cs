0497172d22c6166f3985cba5142ed0e44f5cbac2
Leonardo D'Antoni <leonardodantoni@Leonardos-MacBook-Pro.local>
2017-04-01 17:05:07 -0300

    ejercicio taller 1 calculo precio de viaje

---
+++ Taller1/EjercicioPrecioViaje/EjercicioPrecioViaje/EjercicioPrecioViaje/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EjercicioPrecioViaje
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int nroCliente, kmRecorridos;
+			double precioViaje;
+
+			Console.WriteLine("Ingrese el nro de cliente y los km recorridos");
+			nroCliente = int.Parse(Console.ReadLine());
+			kmRecorridos = int.Parse(Console.ReadLine());
+
+			if (kmRecorridos<5)
+			{
+				precioViaje = 30;
+			}else
+			{
+				precioViaje = kmRecorridos * 10;
+			}
+
+			if (kmRecorridos>50)
+			{
+				precioViaje = precioViaje * 0.9;
+			}
+
+			Console.WriteLine("El precio del viaje es $" + precioViaje);
+			Console.ReadKey();
+		}
+	}
+}
